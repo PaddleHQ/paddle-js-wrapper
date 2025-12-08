7e60bf556200050f6c08e3e3f8d6414ea51876f4
Dan Billson <dbillson@outlook.com>
2025-12-08 15:07:34 +0000

    Checkout payment error (#113)
    * feat: update payment error shape
    
    * chore: add changelog entry
    
    * fix: Align package and GitHub release versions
    
    ---------
    
    Co-authored-by: Dan Billson <daniel.billson@paddle.com>
    Co-authored-by: David Grayston <david.grayston@paddle.com>

--- types/checkout/events.d.ts
+++ types/checkout/events.d.ts
@@ -16,6 +16,7 @@ export enum CheckoutEventNames {
   CHECKOUT_PAYMENT_SELECTED = 'checkout.payment.selected',
   CHECKOUT_PAYMENT_INITIATED = 'checkout.payment.initiated',
   CHECKOUT_PAYMENT_FAILED = 'checkout.payment.failed',
+  CHECKOUT_PAYMENT_ERROR = 'checkout.payment.error',
   CHECKOUT_DISCOUNT_APPLIED = 'checkout.discount.applied',
   CHECKOUT_DISCOUNT_REMOVED = 'checkout.discount.removed',
   CHECKOUT_UPSELL_CANCELED = 'checkout.upsell.canceled',
@@ -163,6 +164,7 @@ export interface CheckoutEventsData {
 }
 
 export interface CheckoutEventError {
+  name: CheckoutEventNames.CHECKOUT_ERROR | CheckoutEventNames.CHECKOUT_PAYMENT_ERROR;
   type: string;
   code: string;
   detail: string;
